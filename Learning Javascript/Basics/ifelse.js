e4297ceecc8e7c3804cbc418dfd526dab955a50c
Ganesh Kachare <kaganesh12@gmail.com>
2025-02-04 21:00:35 +0530

    Learned if-else and switch in Javascript

---
+++ Learning Javascript/Basics/ifelse.js
@@ -0,0 +1,27 @@
+// Cotrol Statements
+
+if (true) {
+    // Execute this.
+} else {
+    // Execute else part.
+}
+
+
+if (true) {
+  // execute this
+} else if (true) {
+  // Execute this
+} else {
+  // Execute this as last
+}
+let ch = 'A'
+switch(ch) {
+    case 'A':
+        console.log("hi");
+        break
+    case 'B':
+        console.log("Bye");
+        break
+    default:
+        console.log("Printing Default");
+}
