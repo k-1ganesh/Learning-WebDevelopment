c9c07245a56d998c9ad3398f3c8bee2068e7b177
Ganesh Kachare <kaganesh12@gmail.com>
2025-02-04 12:43:05 +0530

    Learned datatypes in JavaScript

---
+++ Learning Javascript/Basics/datatypes.js
@@ -0,0 +1,17 @@
+let num = 10  // number
+let str = "Ganesh" // string
+let check = true // boolean
+let isthere // undefined.
+let nothing = null // Datatype of null is object.
+
+console.log(typeof num);
+console.log(typeof str)
+console.log(typeof check)
+console.log(typeof isthere)
+console.log(typeof nothing)
+
+console.log( num);
+console.log( str)
+console.log( check)
+console.log( isthere)
+console.log( nothing)
