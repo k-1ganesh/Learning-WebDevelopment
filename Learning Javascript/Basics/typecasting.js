e503fa7907ded8970c66cdfcafb22596fb51e24b
Ganesh Kachare <kaganesh12@gmail.com>
2025-02-04 12:55:00 +0530

    Learned Typecasting in Javascript

---
+++ Learning Javascript/Basics/typecasting.js
@@ -0,0 +1,10 @@
+// Converting to number.
+
+console.log(Number("12")) // value: 12 , Datatype: number 
+console.log(Number("12abc")) // value: NaN , datatype : number 
+console.log(Number(undefined)) // value : NaN , datatype : number 
+
+// Converting to string .
+console.log(String(12))
+// Converting to boolean 
+console.log(Boolean(1)) 
